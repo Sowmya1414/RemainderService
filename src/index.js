5fe646ad275c2551159a01ccf2e1de4ae4f4018d
SOWMYA <sowmyasriramula216@gmail.com>
2023-09-25 19:48:36 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const { PORT } = require('./config/serverConfig')
 
-const {sendBasicEmail}=require('./services/email-service')
-
+// const {sendBasicEmail}=require('./services/email-service')
+// const cron=require('node-cron')
 const setupAndStartServer=()=>{
     const app=express()
     app.use(bodyParser.json())
@@ -11,12 +11,17 @@ const setupAndStartServer=()=>{
 
     app.listen(PORT,()=>{
         console.log(`Server started at port ${PORT}`);
-        sendBasicEmail(
-            'support@admin.com',
-            'sowmyasriramula216@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'sowmyasriramula216@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // )
+
+
+        // cron.schedule('*/2 * * * *', () => {
+        //     console.log('running a task every two minutes');
+        //   });
     })
 }
 
